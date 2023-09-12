5fe7645279b7558a26cd7503b4770a6b55a92431
Mason Guaman <masonstalin@gmail.com>
2023-09-12 13:46:12 -0400

    update

--- Lab - Operators.playground/Pages/2. App Exercise - Fitness Calculations.xcplaygroundpage/Contents.swift
+++ Lab - Operators.playground/Pages/2. App Exercise - Fitness Calculations.xcplaygroundpage/Contents.swift
@@ -13,13 +13,19 @@ let averageHR = addedHR/3
 print(averageHR)
 
 //:  Now create three more constants, `heartRate1D`, `heartRate2D`, and `heartRate3D`, equal to the same values as `heartRate1`, `heartRate2`, and `heartRate3`. These new constants should be of type `Double`. Create a constant `addedHRD` equal to the sum of all three heart rates. Create a constant called `averageHRD` that equals the `addedHRD` divided by 3 to get the average of your new heart rate constants. Print the result. Does this differ from your previous average? Why or why not?
-let hearRate1D = Double(heartRate1)
-let hearRate2D = Double(heartRate2)
+let heartRate1D = Double(heartRate1)
+let heartRate2D = Double(heartRate2)
 let heartRate3D = Double(heartRate3)
-let addedHRD = heartRate1D + heartRate2D + heartRate3D
+let addedHRD = (heartRate1D + heartRate2D + heartRate3D)
+let averageHRD = addedHRD / 3
+print(averageHRD)
 
+/* The answers do not differ, the only difference is that one is an int and the other is a double. */
 //:  Imagine that partway through the day a user has taken 3,467 steps out of the 10,000 step goal. Create constants `steps` and `goal`. Both will need to be of type `Double` so that you can perform accurate calculations. `steps` should be assigned the value 3,467, and `goal` should be assigned 10,000. Create a constant `percentOfGoal` that equals an expression that evaluates to the percent of the goal that has been achieved so far.
-
+let steps: Double = 3_467
+let goals: Double = 10_000
+let percentofGoal = (steps / goals) * 100
+print(percentofGoal)
 
 /*:
 [Previous](@previous)  |  page 2 of 8  |  [Next: Exercise - Compound Assignment](@next)
